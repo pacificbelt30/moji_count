39e89085d8f4d0ff58d144a762b7a7386bef3787
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2021-11-18 18:57:41 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # moji_count
-文字のカウントがしたいだけ
+文字のカウントがしたいだけ(英単語を1文字としてカウントする)
+
+[ここ]()
+
+inspired by [すぐ使える便利ツール(一般)](https://sundryst.com/convenienttool/index.html), [英単語カウンター](https://tool.stabucky.com/word_counter.htm)
---
+++ static/js/index.js
@@ -0,0 +1,22 @@
+// 字数カウント 
+// inc_lf: カウントに改行文字を含めるか
+// inc_space: カウントにスペースを含めるか
+function char_count(inc_lf,inc_space){
+  textarea = document.querySelector('#textarea');
+  sentences = textarea.value;
+  sentences = sentences.replace(/[a-z,A-Z]+/g,'\*');
+  if (!inc_space) {
+    sentences = sentences.replace(/ +/g,'');
+  }
+  if (!inc_lf) {
+    sentences = sentences.replace(/\n+/g,'');
+  }
+  return sentences.length;
+}
+
+function onChangeText(){
+  document.querySelector('#raw_count').value = char_count(true,true);
+  document.querySelector('#ns_count').value = char_count(true,false);
+  document.querySelector('#nr_count').value = char_count(false,true);
+  document.querySelector('#nsr_count').value = char_count(false,false);
+}
